639681a9e3f56324389415e1c3df9416dfeae417
alexei.s.lazarev <alexei.s.lazarev@gmail.com>
2024-05-18 19:13:16 +0300

    sum_linked_lists1.py refactored tuple -> dictionary

--- dsa1/task1/sum_linked_lists1.py
+++ dsa1/task1/sum_linked_lists1.py
@@ -1,8 +1,9 @@
 from task1 import LinkedList, Node
-from typing import List, Tuple
+from typing import List, Dict, Union
 import unittest
 
-def sum_linked_lists(ll1: LinkedList, ll2: LinkedList) -> Tuple[List[int], str]:
+
+def sum_linked_lists(ll1: LinkedList, ll2: LinkedList) -> Dict[str, Union[List[int], str]]:
     """
     The function takes two linked lists and if their lengths are equal,
     returns a new list where each element is the sum of corresponding elements from the input lists.
@@ -11,14 +12,14 @@ def sum_linked_lists(ll1: LinkedList, ll2: LinkedList) -> Tuple[List[int], str]:
     """
 
     if ll1 is None or ll2 is None:
-        return [], "Bad data."
+        return {"value": [], "message": "Bad data."}
 
     l1: int = ll1.len()
     l2: int = ll2.len()
 
-    if l1 != l2: return [], "Lengths of lists are not equal."
-    if l1 == 0: return [], "Lists are empty."
-    return [a + b for a, b in zip(ll1.list_vals(), ll2.list_vals())], "Done."
+    if l1 != l2: return {"value": [], "message": "Lengths of lists are not equal."}
+    if l1 == 0: return {"value": [], "message": "Lists are empty."}
+    return {"value": [a + b for a, b in zip(ll1.list_vals(), ll2.list_vals())], "message": "Done."}
 
 
 class TestSumLinkedLists(unittest.TestCase):
@@ -34,9 +35,9 @@ class TestSumLinkedLists(unittest.TestCase):
         ll2.add_in_tail(Node(5))
         ll2.add_in_tail(Node(6))
 
-        result, message = sum_linked_lists(ll1, ll2)
-        self.assertEqual(result, [5, 7, 9])
-        self.assertEqual(message, "Done.")
+        result = sum_linked_lists(ll1, ll2)
+        self.assertEqual(result["value"], [5, 7, 9])
+        self.assertEqual(result["message"], "Done.")
 
     def test_sum_different_length(self):
         ll1 = LinkedList()
@@ -49,35 +50,33 @@ class TestSumLinkedLists(unittest.TestCase):
         ll2.add_in_tail(Node(5))
         ll2.add_in_tail(Node(6))
 
-        result, message = sum_linked_lists(ll1, ll2)
-        self.assertEqual(result, [])
-        self.assertEqual(message, "Lengths of lists are not equal.")
+        result = sum_linked_lists(ll1, ll2)
+        self.assertEqual(result["value"], [])
+        self.assertEqual(result["message"], "Lengths of lists are not equal.")
 
     def test_sum_empty_lists(self):
         ll1 = LinkedList()
         ll2 = LinkedList()
 
-        result, message = sum_linked_lists(ll1, ll2)
-        self.assertEqual(result, [])
-        self.assertEqual(message, "Lists are empty.")
+        result = sum_linked_lists(ll1, ll2)
+        self.assertEqual(result["value"], [])
+        self.assertEqual(result["message"], "Lists are empty.")
 
     def test_sum_none_list(self):
         ll1 = None
         ll2 = LinkedList()
 
-        result, message = sum_linked_lists(ll1, ll2)
-        self.assertEqual(result, [])
-        self.assertEqual(message, "Bad data.")
+        result = sum_linked_lists(ll1, ll2)
+        self.assertEqual(result["value"], [])
+        self.assertEqual(result["message"], "Bad data.")
 
         ll1 = LinkedList()
         ll2 = None
 
-        result, message = sum_linked_lists(ll1, ll2)
-        self.assertEqual(result, [])
-        self.assertEqual(message, "Bad data.")
+        result = sum_linked_lists(ll1, ll2)
+        self.assertEqual(result["value"], [])
+        self.assertEqual(result["message"], "Bad data.")
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
