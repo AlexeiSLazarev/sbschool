83e67e3bb506bbe69015d4cc574438bfa04a1fae
alexei.s.lazarev <alexei.s.lazarev@gmail.com>
2024-05-22 22:25:54 +0300

    Bumped to "Некорректное удаление без изменения размера буфера"

--- dsa1/task3/test_task3.py
+++ dsa1/task3/test_task3.py
@@ -3,21 +3,20 @@ from task3 import DynArray
 
 
 class TestDynArrayDelete(unittest.TestCase):
-    def setUp(self):
-        self.dyn_array = DynArray()
+    def setUp(self) -> None:
+        self.dyn_array: DynArray = DynArray()
         for i in range(16):
             self.dyn_array.append(i)
         self.assertEqual(self.dyn_array[0], 0)
         self.assertEqual(self.dyn_array.capacity, 16)
 
-    def test_delete_from_empty_array(self):
-        da = DynArray()
+    def test_delete_from_empty_array(self) -> None:
+        da: DynArray = DynArray()
         with self.assertRaises(IndexError):
             da.delete(0)
 
-    # it was the first bug
-    def test_delete_only_one(self):
-        da = DynArray()
+    def test_delete_only_one(self) -> None:
+        da: DynArray = DynArray()
         da.append(0)
         da.delete(0)
         self.assertEqual(da.list_vals(), [])
@@ -28,8 +27,8 @@ class TestDynArrayDelete(unittest.TestCase):
         with self.assertRaises(IndexError):
             da.delete(1)
 
-    def test_delete_many_to_one_one_to_zero(self):
-        da = DynArray()
+    def test_delete_many_to_one_one_to_zero(self) -> None:
+        da: DynArray = DynArray()
         da.append(0)
         da.append(1)
         da.delete(1)
@@ -37,29 +36,29 @@ class TestDynArrayDelete(unittest.TestCase):
         da.delete(0)
         self.assertListEqual(da.list_vals(), [])
 
-    def test_delete_from_start(self):
+    def test_delete_from_start(self) -> None:
         self.dyn_array.delete(0)
         self.assertEqual(self.dyn_array.list_vals(), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
         self.assertEqual(len(self.dyn_array), 15)
         self.assertEqual(self.dyn_array.capacity, 16)
 
-    def test_delete_from_middle(self):
+    def test_delete_from_middle(self) -> None:
         self.dyn_array.delete(4)
         self.assertEqual(self.dyn_array.list_vals(), [0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
         self.assertEqual(len(self.dyn_array), 15)
         self.assertEqual(self.dyn_array.capacity, 16)
 
-    def test_delete_from_end(self):
+    def test_delete_from_end(self) -> None:
         self.dyn_array.delete(self.dyn_array.count - 1)
-        self.assertEqual(self.dyn_array.list_vals(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, ])
+        self.assertEqual(self.dyn_array.list_vals(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
         self.assertEqual(len(self.dyn_array), 15)
         self.assertEqual(self.dyn_array.capacity, 16)
 
-    def test_delete_invalid_high_index(self):
+    def test_delete_invalid_high_index(self) -> None:
         with self.assertRaises(IndexError):
             self.dyn_array.delete(100)
 
-    def test_delete_invalid_negative_index(self):
+    def test_delete_invalid_negative_index(self) -> None:
         with self.assertRaises(IndexError):
             self.dyn_array.delete(-1)
 
@@ -71,12 +70,11 @@ class TestDynArrayDelete(unittest.TestCase):
         self.assertListEqual(da.list_vals(), [0, 1, 2, 3, 4, 6, 7, 8, 9])
         self.assertEqual(da.capacity, 16)
 
-    def test_delete_series_without_resize(self):
-        da = DynArray()
-        num_el = 16
+    def test_delete_series_without_resize(self) -> None:
+        da: DynArray = DynArray()
+        num_el: int = 16
         for i in range(num_el):
             da.append(i)
-
         for i in range(num_el):
             da.delete(0)
         self.assertListEqual(da.list_vals(), [])
@@ -93,12 +91,11 @@ class TestDynArrayDelete(unittest.TestCase):
         self.assertListEqual(da.list_vals(), [100])
         self.assertEqual(da.capacity, 16)
 
-    def test_delete_series_with_resize(self):
-        da = DynArray()
+    def test_delete_series_with_resize(self) -> None:
+        da: DynArray = DynArray()
         for i in range(32):
             da.append(i)
         self.assertEqual(da.capacity, 32)
-
         for i in range(32):
             da.delete(0)
         self.assertListEqual(da.list_vals(), [])
@@ -133,19 +130,17 @@ class TestDynArrayDelete(unittest.TestCase):
 
 class TestDynArray(unittest.TestCase):
 
-    def test_init(self):
-        da = DynArray()
+    def test_init(self) -> None:
+        da: DynArray = DynArray()
         self.assertEqual(da.count, 0)
         self.assertEqual(da.capacity, 16)
 
-    def test_insert(self):
-        da = DynArray()
+    def test_insert(self) -> None:
+        da: DynArray = DynArray()
         da.append(0)
         da.append(1)
-
         da.insert(0, 2)
         self.assertListEqual(da.list_vals(), [2, 0, 1])
-
         with self.assertRaises(IndexError):
             da.insert(-1, 0)
         with self.assertRaises(IndexError):
@@ -169,3 +164,4 @@ class TestDynArray(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
