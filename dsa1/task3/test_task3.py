9c843f282af17bcc39c89d6722eb48cd63503c3f
alexei.s.lazarev <alexei.s.lazarev@gmail.com>
2024-05-22 22:23:16 +0300

    Bumped to "Некорректное удаление без изменения размера буфера"

--- dsa1/task3/test_task3.py
+++ dsa1/task3/test_task3.py
@@ -1,4 +1,3 @@
-import random
 import unittest
 from task3 import DynArray
 
@@ -52,7 +51,7 @@ class TestDynArrayDelete(unittest.TestCase):
 
     def test_delete_from_end(self):
         self.dyn_array.delete(self.dyn_array.count - 1)
-        self.assertEqual(self.dyn_array.list_vals(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,])
+        self.assertEqual(self.dyn_array.list_vals(), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, ])
         self.assertEqual(len(self.dyn_array), 15)
         self.assertEqual(self.dyn_array.capacity, 16)
 
@@ -64,6 +63,14 @@ class TestDynArrayDelete(unittest.TestCase):
         with self.assertRaises(IndexError):
             self.dyn_array.delete(-1)
 
+    def test_delete_small_series_without_resize(self) -> None:
+        da: DynArray = DynArray()
+        for i in range(10):
+            da.append(i)
+        da.delete(5)
+        self.assertListEqual(da.list_vals(), [0, 1, 2, 3, 4, 6, 7, 8, 9])
+        self.assertEqual(da.capacity, 16)
+
     def test_delete_series_without_resize(self):
         da = DynArray()
         num_el = 16
@@ -73,6 +80,17 @@ class TestDynArrayDelete(unittest.TestCase):
         for i in range(num_el):
             da.delete(0)
         self.assertListEqual(da.list_vals(), [])
+        self.assertEqual(da.capacity, 16)
+
+    def test_delete_to_empty_and_add_again(self) -> None:
+        da: DynArray = DynArray()
+        for i in range(10):
+            da.append(i)
+        for _ in range(10):
+            da.delete(0)
+        self.assertListEqual(da.list_vals(), [])
+        da.append(100)
+        self.assertListEqual(da.list_vals(), [100])
         self.assertEqual(da.capacity, 16)
 
     def test_delete_series_with_resize(self):
@@ -86,6 +104,33 @@ class TestDynArrayDelete(unittest.TestCase):
         self.assertListEqual(da.list_vals(), [])
         self.assertEqual(da.capacity, 16)
 
+    def test_delete_multiple_elements_successively(self) -> None:
+        da: DynArray = DynArray()
+        for i in range(20):
+            da.append(i)
+        for _ in range(5):
+            da.delete(0)
+        self.assertListEqual(da.list_vals(), list(range(5, 20)))
+
+    def test_delete_causing_capacity_reduction(self) -> None:
+        da: DynArray = DynArray()
+        for i in range(32):
+            da.append(i)
+        for _ in range(25):
+            da.delete(0)
+        self.assertListEqual(da.list_vals(), list(range(25, 32)))
+        self.assertEqual(da.capacity, 16)
+
+    def test_delete_every_other_element(self) -> None:
+        da: DynArray = DynArray()
+        for i in range(10):
+            da.append(i)
+        for i in range(5):
+            da.delete(i)
+        self.assertListEqual(da.list_vals(), [1, 3, 5, 7, 9])
+        self.assertEqual(da.capacity, 16)
+
+
 class TestDynArray(unittest.TestCase):
 
     def test_init(self):
@@ -97,25 +142,21 @@ class TestDynArray(unittest.TestCase):
         da = DynArray()
         da.append(0)
         da.append(1)
-        # Вставка элемента, когда размер буфера не превышен.
+
         da.insert(0, 2)
         self.assertListEqual(da.list_vals(), [2, 0, 1])
 
-        # Попытка вставки элемента в недопустимую позицию.
         with self.assertRaises(IndexError):
             da.insert(-1, 0)
         with self.assertRaises(IndexError):
             da.insert(12, 0)
 
-        # Вставка элемента, i == count
-        # Вставка элемента, когда размер буфера превышен.
         da = DynArray()
         da.append(0)
         da.append(1)
         da.insert(2, 2)
         self.assertListEqual(da.list_vals(), [0, 1, 2])
 
-        # Вставка элемента, когда размер буфера превышен.
         da = DynArray()
         for i in range(16):
             da.append(i)
