9750f2f03276f6abedfbef61c47c49d5c33f2d8b
alexei.s.lazarev <alexei.s.lazarev@gmail.com>
2024-05-27 14:06:19 +0300

    Task 7 complete.

---
+++ dsa1/task7/task7.py
@@ -0,0 +1,139 @@
+from typing import Any, List
+
+
+class Node:
+    def __init__(self, value: Any = None):
+        self.value: Any = value
+        self.prev: 'Node' = None
+        self.next: 'Node' = None
+
+
+class DummyNode(Node):
+    def __init__(self):
+        super().__init__()
+
+
+class OrderedList:
+    def __init__(self, asc: bool = True):
+        self.init()
+        self.__ascending = asc
+
+    def init(self):
+        self.start_node: Node = DummyNode()
+        self.start_node.prev = self.start_node
+        self.start_node.next = self.start_node
+        self.list_length: int = 0
+
+    def compare(self, v1: Any, v2: Any) -> int:
+        if v1 < v2:
+            return -1
+        if v1 > v2:
+            return 1
+        return 0
+
+    def insert_new_node_after(self, new_node: Node, left_node: Node):
+        right_node: Node = left_node.next
+
+        left_node.next = new_node
+        new_node.next = right_node
+        new_node.prev = left_node
+        right_node.prev = new_node
+
+        self.list_length += 1
+
+    def insert_new_node_before(self, new_node: Node, right_node: Node):
+        left_node: Node = right_node.prev
+
+        left_node.next = new_node
+        new_node.next = right_node
+        new_node.prev = left_node
+        right_node.prev = new_node
+
+        self.list_length += 1
+
+    def add(self, value: Any):
+        new_node = Node(value)
+        if self.len() == 0:
+            self.insert_new_node_after(new_node, self.start_node)
+        else:
+            if self.__ascending:
+                current_node: Node = self.start_node.next
+                while not isinstance(current_node, DummyNode):
+                    comp_res: int = self.compare(new_node.value, current_node.value)
+                    if comp_res == -1 or comp_res == 0:
+                        self.insert_new_node_before(new_node, current_node)
+                        return
+                    current_node = current_node.next
+                self.insert_new_node_before(new_node, self.start_node)
+            else:
+                current_node: Node = self.start_node.prev
+                while not isinstance(current_node, DummyNode):
+                    comp_res: int = self.compare(new_node.value, current_node.value)
+                    if comp_res == -1 or comp_res == 0:
+                        self.insert_new_node_after(new_node, current_node)
+                        return
+                    current_node = current_node.prev
+                self.insert_new_node_after(new_node, self.start_node)
+
+    def find(self, val: Any) -> Node:
+        current_node: Node = self.start_node.next
+        while not isinstance(current_node, DummyNode):
+            comp_res = self.compare(current_node.value, val)
+            if comp_res == 0:
+                return current_node
+            if self.__ascending and comp_res == 1:
+                return None
+            if not self.__ascending and comp_res == -1:
+                return None
+            current_node = current_node.next
+        return None
+
+    def delete(self, val: Any):
+        current_node: Node = self.start_node.next
+        while not isinstance(current_node, DummyNode):
+            if current_node.value == val:
+                current_node.prev.next = current_node.next
+                current_node.next.prev = current_node.prev
+                self.list_length -= 1
+                return
+            current_node = current_node.next
+
+    def clean(self, asc: bool):
+        self.__ascending = asc
+        self.init()
+
+    def __len__(self) -> int:
+        return self.list_length
+
+    def len(self) -> int:
+        return self.list_length
+
+    def get_all(self) -> List[Node]:
+        r = []
+        node = self.start_node.next
+        while not isinstance(node, DummyNode):
+            r.append(node)
+            node = node.next
+        return r
+
+    def list_vals(self) -> List[Any]:
+        return_list: List[int] = []
+        current_node: Node = self.start_node.next
+        while not isinstance(current_node, DummyNode):
+            return_list.append(current_node.value)
+            current_node = current_node.next
+        return return_list
+
+
+class OrderedStringList(OrderedList):
+    def __init__(self, asc: bool = True):
+        super(OrderedStringList, self).__init__(asc)
+
+    def compare(self, v1: str, v2: str):
+        v1 = v1.strip()
+        v2 = v2.strip()
+        if v1 < v2:
+            return -1
+        if v1 > v2:
+            return 1
+        return 0
---
+++ dsa1/task7/test_task7.py
@@ -0,0 +1,378 @@
+import unittest
+from task7 import OrderedList, OrderedStringList
+
+
+class TestOrderedList(unittest.TestCase):
+
+    def test_add_val_to_empty(self):
+        ord_list = OrderedList()
+        ord_list.add(1)
+        self.assertEqual(ord_list.len(), 1)
+        self.assertListEqual(ord_list.list_vals(), [1])
+
+    def test_add_same_vals(self):
+        ord_list = OrderedList()
+        ord_list.add(1)
+        ord_list.add(1)
+        self.assertEqual(ord_list.len(), 2)
+        self.assertListEqual(ord_list.list_vals(), [1, 1])
+
+        ord_list.add(1)
+        ord_list.add(1)
+        self.assertEqual(ord_list.len(), 4)
+        self.assertListEqual(ord_list.list_vals(), [1, 1, 1, 1])
+
+    def test_asc_desc(self):
+        ord_list = OrderedList(True)
+        ord_list.add(1)
+        ord_list.add(4)
+        ord_list.add(2)
+        ord_list.add(3)
+        self.assertEqual(ord_list.len(), 4)
+        self.assertListEqual(ord_list.list_vals(), [1, 2, 3, 4])
+
+        ord_list = OrderedList(False)
+        ord_list.add(1)
+        ord_list.add(4)
+        ord_list.add(2)
+        ord_list.add(3)
+        self.assertEqual(ord_list.len(), 4)
+        self.assertListEqual(ord_list.list_vals(), [4, 3, 2, 1])
+
+    def test_delete(self):
+        ord_list = OrderedList(True)
+        ord_list.add(1)
+        ord_list.add(4)
+        ord_list.add(2)
+        ord_list.add(3)
+        ord_list.delete(3)
+        self.assertListEqual(ord_list.list_vals(), [1, 2, 4])
+        self.assertEqual(ord_list.len(), 3)
+
+    def test_find(self):
+        ord_list = OrderedList(True)
+        ord_list.add(1)
+        ord_list.add(4)
+        ord_list.add(2)
+        ord_list.add(3)
+        node = ord_list.find(3)
+        self.assertIsNotNone(node)
+        self.assertEqual(node.value, 3)
+
+        node = ord_list.find(5)
+        self.assertIsNone(node)
+
+    def test_clean(self):
+        ord_list = OrderedList(True)
+        ord_list.add(1)
+        ord_list.add(4)
+        ord_list.clean(False)
+        self.assertEqual(ord_list.len(), 0)
+        self.assertListEqual(ord_list.list_vals(), [])
+
+    def test_get_all(self):
+        ord_list = OrderedList(True)
+        ord_list.add(1)
+        ord_list.add(4)
+        nodes = ord_list.get_all()
+        self.assertEqual(len(nodes), 2)
+        self.assertEqual(nodes[0].value, 1)
+        self.assertEqual(nodes[1].value, 4)
+
+    def test_add_to_one_element(self):
+        ord_list = OrderedList()
+        ord_list.add(2)
+        ord_list.add(1)
+        self.assertEqual(ord_list.len(), 2)
+        self.assertListEqual(ord_list.list_vals(), [1, 2])
+
+    def test_add_to_many_elements(self):
+        ord_list = OrderedList()
+        ord_list.add(3)
+        ord_list.add(1)
+        ord_list.add(2)
+        self.assertEqual(ord_list.len(), 3)
+        self.assertListEqual(ord_list.list_vals(), [1, 2, 3])
+
+    def test_find_in_empty(self):
+        ord_list = OrderedList()
+        self.assertIsNone(ord_list.find(1))
+
+    def test_find_in_one_element(self):
+        ord_list = OrderedList()
+        ord_list.add(1)
+        self.assertIsNotNone(ord_list.find(1))
+        self.assertIsNone(ord_list.find(2))
+
+    def test_find_in_many_elements(self):
+        ord_list = OrderedList()
+        ord_list.add(1)
+        ord_list.add(2)
+        ord_list.add(3)
+        self.assertIsNotNone(ord_list.find(2))
+        self.assertIsNone(ord_list.find(4))
+
+    def test_delete_from_empty(self):
+        ord_list = OrderedList()
+        ord_list.delete(1)
+        self.assertEqual(ord_list.len(), 0)
+        self.assertListEqual(ord_list.list_vals(), [])
+
+    def test_delete_from_one_element(self):
+        ord_list = OrderedList()
+        ord_list.add(1)
+        ord_list.delete(1)
+        self.assertEqual(ord_list.len(), 0)
+        self.assertListEqual(ord_list.list_vals(), [])
+
+    def test_delete_from_many_elements(self):
+        ord_list = OrderedList()
+        ord_list.add(1)
+        ord_list.add(2)
+        ord_list.add(3)
+        ord_list.delete(2)
+        self.assertEqual(ord_list.len(), 2)
+        self.assertListEqual(ord_list.list_vals(), [1, 3])
+        ord_list.delete(1)
+        self.assertEqual(ord_list.len(), 1)
+        self.assertListEqual(ord_list.list_vals(), [3])
+        ord_list.delete(3)
+        self.assertEqual(ord_list.len(), 0)
+        self.assertListEqual(ord_list.list_vals(), [])
+
+    def test_get_all_empty(self):
+        ord_list = OrderedList()
+        self.assertEqual(len(ord_list.get_all()), 0)
+
+    def test_get_all_one_element(self):
+        ord_list = OrderedList()
+        ord_list.add(1)
+        nodes = ord_list.get_all()
+        self.assertEqual(len(nodes), 1)
+        self.assertEqual(nodes[0].value, 1)
+
+    def test_get_all_many_elements(self):
+        ord_list = OrderedList()
+        ord_list.add(1)
+        ord_list.add(2)
+        ord_list.add(3)
+        nodes = ord_list.get_all()
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[0].value, 1)
+        self.assertEqual(nodes[1].value, 2)
+        self.assertEqual(nodes[2].value, 3)
+
+    def test_add_ascending(self):
+        ord_list = OrderedList(True)
+        ord_list.add(3)
+        ord_list.add(1)
+        ord_list.add(2)
+        self.assertEqual(ord_list.len(), 3)
+        self.assertListEqual(ord_list.list_vals(), [1, 2, 3])
+
+    def test_add_descending(self):
+        ord_list = OrderedList(False)
+        ord_list.add(3)
+        ord_list.add(1)
+        ord_list.add(2)
+        self.assertEqual(ord_list.len(), 3)
+        self.assertListEqual(ord_list.list_vals(), [3, 2, 1])
+
+    # Find tests
+    def test_find_ascending(self):
+        ord_list = OrderedList(True)
+        ord_list.add(1)
+        ord_list.add(2)
+        ord_list.add(3)
+        self.assertIsNotNone(ord_list.find(2))
+        self.assertIsNone(ord_list.find(4))
+
+    def test_find_descending(self):
+        ord_list = OrderedList(False)
+        ord_list.add(1)
+        ord_list.add(2)
+        ord_list.add(3)
+        self.assertIsNotNone(ord_list.find(2))
+        self.assertIsNone(ord_list.find(0))
+
+    # Delete tests
+    def test_delete_ascending(self):
+        ord_list = OrderedList(True)
+        ord_list.add(1)
+        ord_list.add(2)
+        ord_list.add(3)
+        ord_list.delete(2)
+        self.assertEqual(ord_list.len(), 2)
+        self.assertListEqual(ord_list.list_vals(), [1, 3])
+
+    def test_delete_descending(self):
+        ord_list = OrderedList(False)
+        ord_list.add(1)
+        ord_list.add(2)
+        ord_list.add(3)
+        ord_list.delete(2)
+        self.assertEqual(ord_list.len(), 2)
+        self.assertListEqual(ord_list.list_vals(), [3, 1])
+
+
+class TestOrderedStringList(unittest.TestCase):
+
+    def test_add_val_to_empty(self):
+        ord_list = OrderedStringList()
+        ord_list.add("Hello")
+        self.assertEqual(ord_list.len(), 1)
+        self.assertListEqual(ord_list.list_vals(), ["Hello"])
+
+    def test_asc_desc(self):
+        ord_list = OrderedStringList()
+        ord_list.add("Hello")
+        ord_list.add("World")
+        self.assertEqual(ord_list.len(), 2)
+        self.assertListEqual(ord_list.list_vals(), ["Hello", "World"])
+
+        ord_list = OrderedStringList(False)
+        ord_list.add("Hello")
+        ord_list.add("World")
+        self.assertEqual(ord_list.len(), 2)
+        self.assertListEqual(ord_list.list_vals(), ["World", "Hello",])
+
+    def test_add_val_to_many_elements(self):
+        ord_list = OrderedStringList()
+        ord_list.add("Banana")
+        ord_list.add("Apple")
+        ord_list.add("Cherry")
+        self.assertEqual(ord_list.len(), 3)
+        self.assertListEqual(ord_list.list_vals(), ["Apple", "Banana", "Cherry"])
+
+    def test_find_in_empty(self):
+        ord_list = OrderedStringList()
+        self.assertIsNone(ord_list.find("Hello"))
+
+    def test_find_in_one_element(self):
+        ord_list = OrderedStringList()
+        ord_list.add("Hello")
+        self.assertIsNotNone(ord_list.find("Hello"))
+        self.assertIsNone(ord_list.find("World"))
+
+    def test_find_in_many_elements(self):
+        ord_list = OrderedStringList()
+        ord_list.add("Apple")
+        ord_list.add("Banana")
+        ord_list.add("Cherry")
+        self.assertIsNotNone(ord_list.find("Banana"))
+        self.assertIsNone(ord_list.find("Date"))
+
+    def test_delete_from_empty(self):
+        ord_list = OrderedStringList()
+        ord_list.delete("Hello")
+        self.assertEqual(ord_list.len(), 0)
+        self.assertListEqual(ord_list.list_vals(), [])
+
+    def test_delete_from_one_element(self):
+        ord_list = OrderedStringList()
+        ord_list.add("Hello")
+        ord_list.delete("Hello")
+        self.assertEqual(ord_list.len(), 0)
+        self.assertListEqual(ord_list.list_vals(), [])
+
+    def test_delete_from_many_elements(self):
+        ord_list = OrderedStringList()
+        ord_list.add("Apple")
+        ord_list.add("Banana")
+        ord_list.add("Cherry")
+        ord_list.delete("Banana")
+        self.assertEqual(ord_list.len(), 2)
+        self.assertListEqual(ord_list.list_vals(), ["Apple", "Cherry"])
+        ord_list.delete("Apple")
+        self.assertEqual(ord_list.len(), 1)
+        self.assertListEqual(ord_list.list_vals(), ["Cherry"])
+        ord_list.delete("Cherry")
+        self.assertEqual(ord_list.len(), 0)
+        self.assertListEqual(ord_list.list_vals(), [])
+
+    def test_get_all_empty(self):
+        ord_list = OrderedStringList()
+        self.assertEqual(len(ord_list.get_all()), 0)
+
+    def test_get_all_one_element(self):
+        ord_list = OrderedStringList()
+        ord_list.add("Hello")
+        nodes = ord_list.get_all()
+        self.assertEqual(len(nodes), 1)
+        self.assertEqual(nodes[0].value, "Hello")
+
+    def test_get_all_many_elements(self):
+        ord_list = OrderedStringList()
+        ord_list.add("Apple")
+        ord_list.add("Banana")
+        ord_list.add("Cherry")
+        nodes = ord_list.get_all()
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(nodes[0].value, "Apple")
+        self.assertEqual(nodes[1].value, "Banana")
+        self.assertEqual(nodes[2].value, "Cherry")
+
+    def test_clean(self):
+        ord_list = OrderedStringList(True)
+        ord_list.add("Apple")
+        ord_list.add("Banana")
+        ord_list.clean(False)
+        self.assertEqual(ord_list.len(), 0)
+        self.assertListEqual(ord_list.list_vals(), [])
+
+    def test_add_ascending(self):
+        ord_list = OrderedStringList(True)
+        ord_list.add("Banana")
+        ord_list.add("Apple")
+        ord_list.add("Cherry")
+        self.assertEqual(ord_list.len(), 3)
+        self.assertListEqual(ord_list.list_vals(), ["Apple", "Banana", "Cherry"])
+
+    def test_add_descending(self):
+        ord_list = OrderedStringList(False)
+        ord_list.add("Banana")
+        ord_list.add("Apple")
+        ord_list.add("Cherry")
+        self.assertEqual(ord_list.len(), 3)
+        self.assertListEqual(ord_list.list_vals(), ["Cherry", "Banana", "Apple"])
+
+    # Find tests
+    def test_find_ascending(self):
+        ord_list = OrderedStringList(True)
+        ord_list.add("Apple")
+        ord_list.add("Banana")
+        ord_list.add("Cherry")
+        self.assertIsNotNone(ord_list.find("Banana"))
+        self.assertIsNone(ord_list.find("Date"))
+
+    def test_find_descending(self):
+        ord_list = OrderedStringList(False)
+        ord_list.add("Apple")
+        ord_list.add("Banana")
+        ord_list.add("Cherry")
+        self.assertIsNotNone(ord_list.find("Banana"))
+        self.assertIsNone(ord_list.find("Aardvark"))
+
+    # Delete tests
+    def test_delete_ascending(self):
+        ord_list = OrderedStringList(True)
+        ord_list.add("Apple")
+        ord_list.add("Banana")
+        ord_list.add("Cherry")
+        ord_list.delete("Banana")
+        self.assertEqual(ord_list.len(), 2)
+        self.assertListEqual(ord_list.list_vals(), ["Apple", "Cherry"])
+
+    def test_delete_descending(self):
+        ord_list = OrderedStringList(False)
+        ord_list.add("Apple")
+        ord_list.add("Banana")
+        ord_list.add("Cherry")
+        ord_list.delete("Banana")
+        self.assertEqual(ord_list.len(), 2)
+        self.assertListEqual(ord_list.list_vals(), ["Cherry", "Apple"])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
